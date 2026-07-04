f70afcbc16df2897eee0901c4bca817e13b279b0
Chawanangwa Kamanga <chawerkamanga@gmail.com>
2026-07-04 22:42:54 +0200

    fix: updated the favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/opt-logo.png' },
+        { rel: 'shortcut icon', type: 'image/png', href: '/opt-logo.png' },
+        { rel: 'apple-touch-icon', href: '/opt-logo.png' }
+      ]
+    }
+  }
 })
